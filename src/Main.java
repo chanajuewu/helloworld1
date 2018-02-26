b3af85a554887c5f385c6c3c2e06a1b36f8de9ab
chanajuewu <31461141@163.com>
2018-02-26 10:44:12 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+
+        System.out.println("我是但发撒旦法否阿什顿飞"
+        );
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test{
     public static void main(String[] args) {
-        System.out.println("hello-word");
+        System.out.println("hello-word111");
     }
 }
